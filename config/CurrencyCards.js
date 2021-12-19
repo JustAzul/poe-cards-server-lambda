ebe2d6e35ca58c5b8e8a0147be2da988da7c67ec
Azul <diego.kfu@hotmail.com>
2021-12-18 21:58:13 -0300

    First Commit

---
+++ config/CurrencyCards.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    Name: 'The Wrath',
+    Reward: 'Chaos Orb',
+    Amount: 10,
+  },
+  {
+    Name: 'Abandoned Wealth',
+    Reward: 'Exalted Orb',
+    Amount: 3,
+  }, {
+    Name: "The Saint's Treasure",
+    Reward: 'Exalted Orb',
+    Amount: 2,
+  }, {
+    Name: 'The Hoarder',
+    Reward: 'Exalted Orb',
+    Amount: 1,
+  }, {
+    Name: 'The Sephirot',
+    Reward: 'Divine Orb',
+    Amount: 10,
+  }, {
+    Name: 'House of Mirrors',
+    Reward: 'Mirror of Kalandra',
+    Amount: 1,
+  }, {
+    Name: 'Seven Years Bad Luck',
+    Reward: 'Mirror Shard',
+    Amount: 1,
+  },
+];
