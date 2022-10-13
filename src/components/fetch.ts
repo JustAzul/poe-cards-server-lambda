da67f7c041ed1c6b38f695dbc395856ad102ab7b
Azul <metalloger@gmail.com>
2022-10-12 22:00:37 -0300

    added leagueOverview

--- src/components/fetch.ts
+++ src/components/fetch.ts
@@ -2,6 +2,7 @@ import { CurrencyOverviewResponse } from '../types/currency-overview-response.ty
 import { ItemOverviewDictionary } from '../types/item-overview-dictionary.type';
 import { ItemOverviewResponse } from '../types/item-overview-response.type';
 import { ItemOverviewType } from '../types/item-overview-types.type';
+import LEAGUE_OVERVIEW_FETCH_LIST from '../constants/fetch-list';
 import { LeagueName } from '../types/league-name.type';
 import { LeagueResponse } from '../types/league-response.type';
 import axios from 'axios';
@@ -25,7 +26,7 @@ export default {
   async itemOverview<OverviewType extends ItemOverviewType>(
     leagueName: LeagueName,
     overviewType: OverviewType,
-  ): Promise< ItemOverviewDictionary[OverviewType][] > {
+  ): Promise<Array<ItemOverviewDictionary[OverviewType]>> {
     console.log(`Fetching ${overviewType} item overview for ${leagueName}`);
 
     const params = {
@@ -66,5 +67,39 @@ export default {
       details: data.currencyDetails,
     };
   },
+
+  async leagueItemsOverview(leagueName: LeagueName) {
+    const overviewsByName = new Map(
+      LEAGUE_OVERVIEW_FETCH_LIST
+        .map(
+          <OverviewType extends ItemOverviewType>(itemOverviewType: OverviewType) => [
+            itemOverviewType,
+            [] as Array<ItemOverviewDictionary[OverviewType]>,
+          ],
+        ),
+    );
+
+    for (let i = 0; i < LEAGUE_OVERVIEW_FETCH_LIST.length; i += 1) {
+      const itemOverviewType = LEAGUE_OVERVIEW_FETCH_LIST[i];
+
+      // eslint-disable-next-line no-await-in-loop
+      const overview = await this.itemOverview(leagueName, itemOverviewType);
+
+      overviewsByName.set(itemOverviewType, overview);
+    }
+
+    return overviewsByName;
+  },
+
+  async leagueOverview(leagueName: LeagueName) {
+    const currencyOverview = await this.currencyOverview(leagueName);
+    const itemsOverview = await this.leagueItemsOverview(leagueName);
+
+    return {
+      currencyOverview,
+      itemsOverview,
+    };
+  },
+
   utils,
 };
