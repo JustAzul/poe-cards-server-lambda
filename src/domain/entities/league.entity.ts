76358ed2df23209fa3434f18d42aa2bda8885fcd
JustAzul <metalloger@gmail.com>
2023-09-23 21:17:25 -0300

    ✨ feat(league.entity.ts): refactor LeagueEntity class to improve encapsulation and add new methods for rule checking 🔥 remove(league.entity.ts): comment out unused properties in LeagueEntityProps to clean up code

--- src/domain/entities/league.entity.ts
+++ src/domain/entities/league.entity.ts
@@ -5,13 +5,13 @@ interface Rule {
 }
 
 interface LeagueEntityProps {
-  ancestorEvent?: boolean;
+  // ancestorEvent?: boolean;
   delveEvent: boolean;
-  description: string;
+  // description: string;
   endAt: string | null;
   id: string;
   realm: string;
-  registerAt: string;
+  // registerAt: string;
   rules: Rule[];
   startAt: string | null;
   timedEvent?: boolean;
@@ -19,5 +19,65 @@ interface LeagueEntityProps {
 }
 
 export default class LeagueEntity {
-  constructor(private readonly props: LeagueEntityProps) {}
+  private readonly props: LeagueEntityProps;
+
+  constructor(props: LeagueEntityProps) {
+    this.props = props;
+  }
+
+  private hasRule(ruleID: string): boolean {
+    return this.rules.some(({ id }) => id === ruleID);
+  }
+
+  isHardcore(): boolean {
+    return this.hasRule('Hardcore');
+  }
+
+  isSSF(): boolean {
+    return this.hasRule('NoParties');
+  }
+
+  isRuthless(): boolean {
+    return this.hasRule('HardMode');
+  }
+
+  shouldFilter(): boolean {
+    if (this.name === 'Standard') {
+      return true;
+    }
+
+    return this.isSSF() || this.isRuthless();
+  }
+
+  get name(): string {
+    return this.props.id;
+  }
+
+  get realm(): string {
+    return this.props.realm;
+  }
+
+  get rules(): Rule[] {
+    return this.props.rules;
+  }
+
+  get rankingURL(): string {
+    return this.props.url;
+  }
+
+  get startAt(): Date | null {
+    return this.props.startAt ? new Date(this.props.startAt) : null;
+  }
+
+  get endAt(): Date | null {
+    return this.props.endAt ? new Date(this.props.endAt) : null;
+  }
+
+  get isTimedEvent(): boolean {
+    return Boolean(this.props.timedEvent);
+  }
+
+  get hasDelveEvent(): boolean {
+    return Boolean(this.props.delveEvent);
+  }
 }
