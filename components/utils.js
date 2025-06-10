fa2929b5f325a1a5dc9e6b5bbe123e25f7812017
Diego (Azul) Ferreira <30638036+JustAzul@users.noreply.github.com>
2025-06-10 19:08:55 -0300

    fix: handle zero exalted in ChaosToExalted

--- components/utils.js
+++ components/utils.js
@@ -11,6 +11,7 @@ async function Delay(DelaySize = 2) {
 }
 
 function ChaosToExalted(ExaltedValue = 0, ChaosValue = 0) {
+  if (ExaltedValue === 0) return 0;
   return parseFloat(parseFloat(ChaosValue / ExaltedValue).toFixed(1));
 }
 
