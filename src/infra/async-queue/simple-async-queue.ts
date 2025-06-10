babdf9c7dbb268e7000ac4d51c6ae1c47d52ea72
Diego (Azul) Ferreira <30638036+JustAzul@users.noreply.github.com>
2025-06-10 20:54:10 -0300

    feat: add optional delay to simple async queue

--- src/infra/async-queue/simple-async-queue.ts
+++ src/infra/async-queue/simple-async-queue.ts
@@ -1,11 +1,24 @@
 import { IAsyncQueue } from '../../application/ports/async-queue.interface';
+import sleep from '../../shared/helpers/sleep.helper';
 
 export default class SimpleAsyncQueue<T> implements IAsyncQueue<T> {
   private lastPromise: Promise<unknown> = Promise.resolve();
 
+  constructor(private readonly delayInMs = 0) {}
+
   async insertAndProcess<R>(job: (input?: T) => Promise<R>): Promise<R> {
-    const result = this.lastPromise.then(() => job());
+    const result = this.lastPromise
+      .then(() => job())
+      .then(async (jobResult) => {
+        if (this.delayInMs > 0) {
+          await sleep(this.delayInMs);
+        }
+
+        return jobResult;
+      });
+
     this.lastPromise = result.catch(() => {});
+
     return result;
   }
 }
