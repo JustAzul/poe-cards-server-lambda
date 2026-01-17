4b6072618c90a21f7df1fb7dddcc18ddf8cf71b9
Diego (Azul) Ferreira <metalloger@gmail.com>
2026-01-17 11:07:15 -0300

    refactor(legacy): remove deprecated Firebase components
    Remove legacy Firebase components that have been replaced by the new
    in-memory data storage layer:
    - components/firestore/firebase.js
    - components/firestore/index.js
    - config/firebase-credentials.js
    
    These were deprecated in earlier commits and are no longer needed.

--- components/firestore/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseAdmin = require('firebase-admin');
-const SERVICE_ACCOUNT = require('../../config/firebase-credentials');
-
-const firebase = firebaseAdmin.initializeApp({
-  credential: firebaseAdmin.credential.cert(SERVICE_ACCOUNT),
-});
-
-module.exports = firebase;
