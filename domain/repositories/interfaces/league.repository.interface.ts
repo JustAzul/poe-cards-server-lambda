748bffeef704d1b93481cfd6b222b71bcbf54302
Diego (Azul) Ferreira <metalloger@gmail.com>
2026-01-21 21:09:15 -0300

    refactor(interfaces): remove documentation from ILeagueRepository
    - Remove redundant comment from getAllLeagues method
    - Simplify interface definition for clarity

--- domain/repositories/interfaces/league.repository.interface.ts
+++ domain/repositories/interfaces/league.repository.interface.ts
@@ -1,8 +1,5 @@
 import { LeagueEntity } from '@domain/entities/league.entity';
 
 export interface ILeagueRepository {
-  /**
-   * Fetches all active leagues from PoE API
-   */
   getAllLeagues(): Promise<LeagueEntity[]>;
 }
