0d54d90b7bf48814c2d8d88e128c0b345dabf69e
Diego (Azul) Ferreira <metalloger@gmail.com>
2026-02-01 18:41:28 -0300

    refactor(infrastructure): update league service interface type references
    Update LeagueEntity type reference to League in league service interface
    to align with domain entity naming conventions.

--- infrastructure/services/interfaces/league.service.interface.ts
+++ infrastructure/services/interfaces/league.service.interface.ts
@@ -1,6 +1,6 @@
-import { LeagueEntity } from '@domain/entities/league.entity';
+import { League } from '@domain/entities/league.entity';
 import { LeagueDataYield } from '@infrastructure/services/league.service';
 
 export interface ILeagueService {
-  fetchBatchLeagueOverview(leagues: LeagueEntity[]): AsyncGenerator<LeagueDataYield>;
+  fetchBatchLeagueOverview(leagues: League[]): AsyncGenerator<LeagueDataYield>;
 }
